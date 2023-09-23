211a740d7fc5f2e03eba8f721e6320928bb2f239
je-woodruff <jeremywoodruff1212@gmail.com>
2023-09-23 16:32:42 -0500

    Added Functionality

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addFunction(View view){
+        try {
+            int num1 = 0;
+            int num2 = 0;
+            EditText myTextField1 = (EditText) findViewById(R.id.int1);
+            EditText myTextField2 = (EditText) findViewById(R.id.int2);
+            num1 = Integer.parseInt(myTextField1.getText().toString());
+            num2 = Integer.parseInt(myTextField2.getText().toString());
+            int answer = num1 + num2;
+            goToActivity(Integer.toString(answer));
+        } catch(NumberFormatException er){
+            goToActivity("Invalid Number");
+        }
+    }
+
+    public void minusFunction(View view){
+        try {
+            int num1 = 0;
+            int num2 = 0;
+            EditText myTextField1 = (EditText) findViewById(R.id.int1);
+            EditText myTextField2 = (EditText) findViewById(R.id.int2);
+            num1 = Integer.parseInt(myTextField1.getText().toString());
+            num2 = Integer.parseInt(myTextField2.getText().toString());
+            int answer = num1 - num2;
+            goToActivity(Integer.toString(answer));
+        } catch(NumberFormatException er){
+            goToActivity("Invalid Number");
+        }
+    }
+
+    public void multFunction(View view){
+        try {
+            int num1 = 0;
+            int num2 = 0;
+            EditText myTextField1 = (EditText) findViewById(R.id.int1);
+            EditText myTextField2 = (EditText) findViewById(R.id.int2);
+            num1 = Integer.parseInt(myTextField1.getText().toString());
+            num2 = Integer.parseInt(myTextField2.getText().toString());
+            int answer = num1 * num2;
+            goToActivity(Integer.toString(answer));
+        } catch(NumberFormatException er){
+            goToActivity("Invalid Number");
+        }
+    }
+
+    public void divFunction(View view){
+        try {
+            int num1 = 0;
+            int num2 = 0;
+            EditText myTextField1 = (EditText) findViewById(R.id.int1);
+            EditText myTextField2 = (EditText) findViewById(R.id.int2);
+            num1 = Integer.parseInt(myTextField1.getText().toString());
+            num2 = Integer.parseInt(myTextField2.getText().toString());
+            if (num2 != 0) {
+                int answer = num1 / num2;
+                goToActivity(Integer.toString(answer));
+            } else {
+                goToActivity("Divide By Zero Error");
+            }
+        } catch(NumberFormatException er){
+            goToActivity("Invalid Number");
+        }
+    }
+
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 }
